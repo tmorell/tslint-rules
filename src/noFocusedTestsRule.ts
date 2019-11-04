07d1f0977906530a3721105bbd82f451128c5656
Teo Morell <teomorell@gmail.com>
2019-11-03 19:04:00 -0500

    Refactor no-focused-tests

--- src/noFocusedTestsRule.ts
+++ src/noFocusedTestsRule.ts
@@ -6,24 +6,26 @@ const RUNNER_AVA = "ava";
 const RUNNER_JASMINE = "jasmine";
 const RUNNER_JEST = "jest";
 const RUNNER_MOCHA = "mocha";
+const validators = {
+    ava: validatorAva,
+    jasmine: validatorJasmine,
+    jest: validatorJest,
+    mocha: validatorMocha
+};
 
 interface Options {
     runner: "ava" | "jasmine" | "jest" | "mocha";
     suffix: string;
-    checks: Array<string>;
+    validator: (node: ts.CallExpression) => boolean;
 }
 
 export class Rule extends Lint.Rules.AbstractRule {
     public static FAILURE_FOCUSED = "Do not commit focused tests.";
-    public static FAILURE_FDESCRIBE = "Favor '.only' over 'fdescribe'.";
-    public static FAILURE_FIT = "Favor '.only' over 'fit'.";
     public static metadata: Lint.IRuleMetadata = {
-        description: "Ensures that no tests are missed.",
-        options: {
-            type: "string",
-            enum: [RUNNER_AVA, RUNNER_JASMINE, RUNNER_JEST, RUNNER_MOCHA]
-        },
-        optionExamples: [[true, RUNNER_AVA], [true, RUNNER_JASMINE], [true, RUNNER_JEST], [true, RUNNER_MOCHA]],
+        description: Rule.FAILURE_FOCUSED,
+        options: { type: "string", enum: [RUNNER_AVA, RUNNER_JASMINE, RUNNER_JEST, RUNNER_MOCHA] },
+        optionExamples: [[true, { runner: RUNNER_AVA }], [true, { runner: RUNNER_JASMINE }],
+        [true, { runner: RUNNER_JEST }], [true, { runner: RUNNER_MOCHA }]],
         optionsDescription: Lint.Utils.dedent`
             Options:
             * \`"${RUNNER_AVA}"\` checks for AVA focused tests.
@@ -42,69 +44,16 @@ export class Rule extends Lint.Rules.AbstractRule {
         if (options.suffix && !sourceFile.fileName.endsWith(options.suffix)) {
             return new Array<Lint.RuleFailure>();
         }
+        options.validator = validators[options.runner];
 
-        let walker: (ctx: Lint.WalkContext<Options>) => void;
-        switch (options.runner) {
-            case RUNNER_AVA:
-                options.checks = ["test.only", "test.serial.only"];
-                walker = walkTwoArgs;
-                break;
-            case RUNNER_JASMINE:
-                options.checks = ["fdescribe", "fit"];
-                walker = walkOneArg;
-                break;
-            case RUNNER_MOCHA:
-                options.checks = ["describe.only", "it.only", "specify.only", "context.only"];
-                walker = walkTwoArgs;
-                break;
-            case RUNNER_JEST:
-                options.checks = ["describe.only", "describe.only.each", "it.only"];
-                walker = walkTwoArgs;
-                break;
-            default:
-                // TODO: Handle configuration error
-                return new Array<Lint.RuleFailure>();
-        }
-
-        if (!possibleMatches(sourceFile, options)) {
-            return new Array<Lint.RuleFailure>();
-        }
-
-        return this.applyWithFunction(sourceFile, walker, options);
+        return this.applyWithFunction(sourceFile, walk, options);
     }
 }
 
-function walkOneArg(ctx: Lint.WalkContext<Options>): void {
-    const walkNode = (node: ts.Node): void => {
-        if (tsutils.isCallExpression(node) &&
-            node.arguments.length === 1 &&
-            tsutils.isIdentifier(node.expression) &&
-            ctx.options.checks.includes(node.expression.getText())
-        ) {
-            ctx.addFailureAtNode(node, Rule.FAILURE_FOCUSED);
-        }
-        ts.forEachChild(node, walkNode);
-    };
-    ts.forEachChild(ctx.sourceFile, walkNode);
-}
-
-function walkTwoArgs(ctx: Lint.WalkContext<Options>): void {
+function walk(ctx: Lint.WalkContext<Options>): void {
     const walkNode = (node: ts.Node): void => {
         if (tsutils.isCallExpression(node)) {
-            const text = node.expression.getText();
-            if (text === "fdescribe") {
-                ctx.addFailureAtNode(node, Rule.FAILURE_FDESCRIBE);
-            } else if (text === "fit") {
-                ctx.addFailureAtNode(node, Rule.FAILURE_FIT);
-            } else if (ctx.options.runner === RUNNER_JEST && isJestTable(node)) {
-                ctx.addFailureAtNode(node, Rule.FAILURE_FOCUSED);
-            } else if (
-                tsutils.isPropertyAccessExpression(node.expression) &&
-                node.arguments.length === 2 &&
-                (tsutils.isStringLiteral(node.arguments[0]) || tsutils.isTemplateLiteral(node.arguments[0])) &&
-                (tsutils.isFunctionExpression(node.arguments[1]) || tsutils.isArrowFunction(node.arguments[1])) &&
-                ctx.options.checks.includes(text)
-            ) {
+            if (ctx.options.validator(node)) {
                 ctx.addFailureAtNode(node, Rule.FAILURE_FOCUSED);
             }
         }
@@ -113,14 +62,67 @@ function walkTwoArgs(ctx: Lint.WalkContext<Options>): void {
     ts.forEachChild(ctx.sourceFile, walkNode);
 }
 
-function isJestTable(node: ts.CallExpression): boolean {
-    return node.expression.getText() === "describe.only.each" &&
-        node.arguments.length === 1 &&
-        tsutils.isArrayLiteralExpression(node.arguments[0])
-        ;
+function validatorAva(node: ts.CallExpression): boolean {
+    return validatorOnly(node, ["test.only", "test.serial.only"]);
 }
 
-function possibleMatches(sourceFile: ts.SourceFile, options: Options): boolean {
-    const text = sourceFile.getFullText();
-    return (options.checks.some((check) => text.indexOf(check) > -1));
+function validatorJasmine(node: ts.CallExpression): boolean {
+    return ["fdescribe", "fit"].includes(node.expression.getText()) &&
+        tsutils.isCallExpression(node) &&
+        node.arguments.length === 1 &&
+        (tsutils.isArrowFunction(node.arguments[0]) || tsutils.isFunctionExpression(node.arguments[0])) &&
+        tsutils.isIdentifier(node.expression);
+}
+
+function validatorJest(node: ts.CallExpression): boolean {
+    // Check Jest tables
+    if (["describe.only.each"].includes(node.expression.getText()) &&
+        node.arguments.length === 1 &&
+        tsutils.isArrayLiteralExpression(node.arguments[0])) {
+        return true;
+    }
+
+    // Check without .only accessor; fdescription, fit
+    if (validatorTest(node, ["fdescribe", "fit"])) {
+        return true;
+    }
+
+    // Check with .only accessor
+    return validatorOnly(node, ["describe.only", "it.only", "specify.only", "context.only"]);
+}
+
+function validatorMocha(node: ts.CallExpression): boolean {
+    return validatorOnly(node, ["describe.only", "it.only", "specify.only", "context.only"]);
+}
+
+/**
+ * Validates tests of form:
+ *      expression.only("string literal", () => { });
+ *      expression.only(`template literal`, () => { });
+ * With .only accessor.
+ *
+ * @param {ts.CallExpression} node
+ * @param {Array<string>} expressions
+ * @returns {boolean}
+ */
+function validatorOnly(node: ts.CallExpression, expressions: Array<string>): boolean {
+    return tsutils.isPropertyAccessExpression(node.expression) &&
+        validatorTest(node, expressions);
+}
+
+/**
+ * Validates tests of form:
+ *      expression("string literal", () => { });
+ *      expression(`template literal`, () => { });
+ * With or without .only accessor.
+ *
+ * @param {ts.CallExpression} node
+ * @param {Array<string>} expressions
+ * @returns {boolean}
+ */
+function validatorTest(node: ts.CallExpression, expressions: Array<string>): boolean {
+    return expressions.includes(node.expression.getText()) &&
+        node.arguments.length === 2 &&
+        (tsutils.isStringLiteral(node.arguments[0]) || tsutils.isTemplateLiteral(node.arguments[0])) &&
+        (tsutils.isArrowFunction(node.arguments[1]) || tsutils.isFunctionExpression(node.arguments[1]));
 }
